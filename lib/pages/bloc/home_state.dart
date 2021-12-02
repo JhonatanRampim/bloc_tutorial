f9b22f7854dbfa5d2064043b035e0f4d5d8197fb
Jhonatan Rampim <jhonatan.rampim@gmail.com>
2021-12-02 10:56:25 -0300

    initial commit

--- lib/pages/bloc/home_state.dart
+++ lib/pages/bloc/home_state.dart
@@ -1,1 +1,23 @@
-class HomeState extends Eq
+import 'package:equatable/equatable.dart';
+
+class HomeState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeErrorState extends HomeState {
+  final String message;
+
+  HomeErrorState({required this.message});
+}
+
+class HomeStateLoaded extends HomeState {
+  final List<String> list;
+
+  HomeStateLoaded({required this.list});
+}
+
+class HomeStateEmptyList extends HomeState {}
